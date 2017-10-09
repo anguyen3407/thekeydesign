8ba8b0ed7f932c1b99b211832b968cf68200374a
Annie  Nguyen <annienguyen@Annies-Air.localdomain>
2017-10-09 10:04:32 -0700

    added video

--- src/components/What/What.js
+++ src/components/What/What.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './What.css';
 import Footer from '../Footer/Footer';
+import video from '../../video/The Key Design Promo - Medium.mp4';
 
 class What extends Component {
   // constructor () {
   //   super () 
-  
+
   //       }
-      
-  //   }
   
   
 
@@ -17,6 +16,14 @@ class What extends Component {
     return (
 
 <div className= 'hi'>
+
+<div className='b-container'>
+<video loop muted preload='auto' autoPlay width='100%' height='40%'>
+<source src={video} type="video/mp4"/>
+</video>
+
+  </div>
+
   <div className='team-container'>
     <div className="team"> 
       <h1 className='h1'>OUR TEAM</h1>
@@ -27,7 +34,6 @@ class What extends Component {
   </div>
 <div className='video-container'>
   <div className='video-box'>
-<iframe src="https://player.vimeo.com/video/235287002" className='video' width="520" height="290" frameBorder="0" allowFullScreen></iframe>
 <div className="what-text">
 <h1 className='what-h1'>What We Do</h1>
 <h2 className='what-h2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
