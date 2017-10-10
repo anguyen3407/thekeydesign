8fad2cb5ac10edbe8c346332f18458ed3db8ca46
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-10-10 10:42:16 -0700

    checkboxes work!

--- src/components/Prices/Prices.js
+++ src/components/Prices/Prices.js
@@ -7,7 +7,16 @@ import { Link } from 'react-router-dom';
 
 class Prices extends Component {
   constructor(){
-    super()
+    super() 
+    this.state = {
+      basic: false,
+      standard: false,
+      twilight: false,
+      drone: false,
+      house: false
+    }
+
+
   }
 
     onToken(token) {
@@ -18,87 +27,93 @@ class Prices extends Component {
       });
     }
 
+
+    toggleCheckbox() {
+      this.setState({standard: !this.state.standard})
+      this.setState({twilight: !this.state.twilight})
+      this.setState({drone: !this.state.drone})
+      this.setState({house: !this.state.house})
+    }
+
+    toggleClick() {
+      this.setState({basic: !this.state.basic})
+    }
+ 
   render() {
 
-  
+  console.log(this.state.basic)
 
 
     return (
-<div className= 'Prices'>
-<div className= 'wrap'>
-<div className='box boxOne'> 
-  <div className='box-One-container'>
-  <h1>Basic Staging Package</h1>
-    <div className = 'includes'>
-      <b> INCLUDES </b> - Staging with furniture in the Living Room, Dining Area, Kitchen, Master Bedroom, Master Bathroom, and Guest Bathroom.
-    <br></br>
-    - Setup Deposit | &nbsp;<b>$600</b>
-    <br></br>
-    - Paid at closing | &nbsp;<b>$1800</b> ( includes takedown w/ up to 3 months inventory rental )
-    <br></br>
-    - Additional rental fee after first 3 months | &nbsp;<b>$400</b> / paid monthly
-    <br></br>
-    - Consultation |<b> &nbsp;75$</b> (price goes up if 100 mi. outside of Salt Lake City)
+  <div className= 'Prices'>
+  <div className= 'wrap'>
+  <div className='box boxOne'> 
+      <div className='box-One-container'>
+          <h1>Basic Staging Package</h1>
+                <div className = 'includes'>
+                    <b> INCLUDES </b> - Staging with furniture in the Living Room, Dining Area, Kitchen, Master Bedroom, Master Bathroom, and Guest Bathroom.
+                    <br></br>
+                    - Setup Deposit | &nbsp;<b>$600</b>
+                    <br></br>
+                    - Paid at closing | &nbsp;<b>$1800</b> ( includes takedown w/ up to 3 months inventory rental )
+                    <br></br>
+                    - Additional rental fee after first 3 months | &nbsp;<b>$400</b> / paid monthly
+                    <br></br>
+                    - Consultation |<b> &nbsp;75$</b> (price goes up if 100 mi. outside of Salt Lake City)
+                </div>
+                    <div className='add-btn-container'>
+                      <button className='addToCart-btn'  type="button" onClick={()=> {this.toggleClick()}}> Add To Cart</button>
+                    </div>
+  </div>
+  </div>
+  <div className='box boxTwo'> 
+  <div className='box-Two-container'>
+    <div className='boxTwoBackground'></div>
+    <div classname='receipt-checkout'>
+      <div className='checkout-btn'>
+          <StripeCheckout
+                    token={this.onToken}
+                    stripeKey={ stripe }
+                    amount={.00}
+          />
+      </div>
     </div>
   </div>
-   </div>
-<div className='box boxTwo'> 
-<div className='box-Two-container'>
-  <div className='boxTwoBackground'></div>
-  <div classname='receipt-checkout'>
-  <div className='checkout-btn'>
-<StripeCheckout
-          token={this.onToken}
-          stripeKey={ stripe }
-          amount={.00}
-        />
-        </div>
   </div>
-</div>
-</div>
-<div className='box boxThree'>
-<div className='box-Three-container'>
- 
-<br></br>
-{/* <input
-                            type="checkbox"
-                            value={label}
-                            checked={isChecked}
-                            onChange={this.toggleCheckbox}
-                        /> */}
-  &nbsp;standard photography | &nbsp;<b>$150</b> &nbsp; &nbsp; &nbsp;
-  
-  
-  <br></br>
-  <input type='checkbox' value='true' name='checkbox'></input> 
-  &nbsp;twilight photography | &nbsp;<b>$350</b> &nbsp; &nbsp; &nbsp;
-  
-   <br></br>
-  <input type='checkbox' value='true' name='checkbox'></input> 
-  &nbsp;drone photography | &nbsp;<b>$250</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-   <br></br>
-  <input type='checkbox' value='true' name='checkbox'></input>
-  &nbsp;house cleaning | &nbsp;<b>$40/hr</b>
-</div>
-</div>
-<div className='box boxFour'> 
-<div className='box-Four-container'>
-  <div className='includes'>
-  <h1>Full Interior Design Package</h1>
-  INCLUDES-
-  <br></br>
-  Our professional design team can help with interior design needs for Businesses, Residential, New Builds, Parade of Homes, etc.
-  <br></br>
-  &nbsp; &nbsp; &nbsp; &nbsp;- Interior Design Consultation | &nbsp;<b>$75/hr</b>
-  <br></br>
- 
-  <Link to='/contacts' className='nav-contacts'> - <b>CONTACT US</b></Link> for pricing
-  
-</div>
-</div>
-</div>
-</div>
-</div>
+  <div className='box boxThree'>
+  <div className='box-Three-container'>
+      <br></br>
+      <input type="checkbox" onChange={()=> {this.toggleCheckbox()}}/>
+      &nbsp;standard photography | &nbsp;<b>$150</b> &nbsp; &nbsp; &nbsp;
+      
+      <br></br>
+      <input type="checkbox" onChange={()=> {this.toggleCheckbox()}}/>
+      &nbsp;twilight photography | &nbsp;<b>$350</b> &nbsp; &nbsp; &nbsp;
+      
+      <br></br>
+      <input type="checkbox" onChange={()=> {this.toggleCheckbox()}}/>
+      &nbsp;drone photography | &nbsp;<b>$250</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      <br></br>
+      <input type="checkbox" onChange={()=> {this.toggleCheckbox()}}/>
+      &nbsp;house cleaning | &nbsp;<b>$40/hr</b>
+  </div>
+  </div>
+  <div className='box boxFour'> 
+  <div className='box-Four-container'>
+      <div className='includes'>
+          <h1>Full Interior Design Package</h1>
+          INCLUDES-
+          <br></br>
+          Our professional design team can help with interior design needs for Businesses, Residential, New Builds, Parade of Homes, etc.
+          <br></br>
+          &nbsp; &nbsp; &nbsp; &nbsp;- Interior Design Consultation | &nbsp;<b>$75/hr</b>
+          <br></br>
+          <Link to='/contacts' className='nav-contacts'> - <b>CONTACT US</b></Link> for pricing
+      </div>
+  </div>
+  </div>
+  </div>
+  </div>
   );
   }
 }
